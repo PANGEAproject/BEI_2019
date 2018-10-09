b5ddf7876a9ed845625256939b06b887244897bb
PANGEAproject <43254659+PANGEAproject@users.noreply.github.com>
2018-10-09 12:47:12 -0500

    Update load_tables.sql

--- Database_Project/load_tables.sql
+++ Database_Project/load_tables.sql
@@ -1,5 +1,7 @@
 mysql -u -p --local-infile
 USE PANGEA_DB
 SET GLOBAL local_infile = "ON";
-LOAD DATA LOCAL INFILE 'parsing_end.txt' INTO TABLE RS FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '/home/luisrs/storage/Project/bin/parsing_end.txt' INTO TABLE RS FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE '/home/luisrs/storage/Project/bin/parsing_end_2.txt' INTO TABLE FREQUENCY_RS_CONTINENT FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 SET GLOBAL local_infile = "OFF";
