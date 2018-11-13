67d8f9eea05771068586170c61b9fdcb2ddea1bd
PANGEAproject <43254659+PANGEAproject@users.noreply.github.com>
2018-11-12 21:42:16 -0600

    Update create_db.sql

--- Database_Project/create_db.sql
+++ Database_Project/create_db.sql
@@ -13,26 +13,9 @@ ENUM('chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr
  strand           ENUM('+','-') ,
  source           ENUM('SNP138','1000GP','SNP138-1000GP') NOT NULL ,
  start_position   INT unsigned ,
+ link_NCBI VARCHAR(100) NOT NULL,
 
 PRIMARY KEY (rs_id)
-);
-
--- FREQUENCY_RS_CONTINENT
-
-CREATE TABLE FREQUENCY_RS_CONTINENT
-(
- rs_id        VARCHAR(15) NOT NULL ,
- frequency_id INT unsigned NOT NULL AUTO_INCREMENT,
- AFR          FLOAT unsigned NOT NULL, 
- AMR          FLOAT unsigned NOT NULL, 
- EAS          FLOAT unsigned NOT NULL, 
- EUR          FLOAT unsigned NOT NULL, 
- SAS          FLOAT unsigned NOT NULL,
- GLOBAL       FLOAT unsigned NOT NULL, 
-
-PRIMARY KEY (frequency_id),
-KEY `fkIdx_28` (rs_id),
-CONSTRAINT `FK_28` FOREIGN KEY `fkIdx_28` (rs_id) REFERENCES RS (rs_id)
 );
 
 -- ALTERNATIVE_ALLELE
@@ -47,6 +30,27 @@ CREATE TABLE ALTERNATIVE_ALLELE
 PRIMARY KEY (id_alternative_allele, rs_id),
 KEY `fkIdx_25` (rs_id),
 CONSTRAINT `FK_25` FOREIGN KEY `fkIdx_25` (rs_id) REFERENCES RS (rs_id)
+);
+
+
+-- FREQUENCY_RS_CONTINENT
+
+CREATE TABLE FREQUENCY_RS_CONTINENT
+(
+ rs_id        VARCHAR(15) NOT NULL ,
+ id_alternative_allele INT NOT NULL,
+ frequency_id INT unsigned NOT NULL AUTO_INCREMENT,
+ AFR          FLOAT unsigned NOT NULL, 
+ AMR          FLOAT unsigned NOT NULL, 
+ EAS          FLOAT unsigned NOT NULL, 
+ EUR          FLOAT unsigned NOT NULL, 
+ SAS          FLOAT unsigned NOT NULL,
+ GLOBAL       FLOAT unsigned NOT NULL, 
+
+PRIMARY KEY (frequency_id,rs_id,id_alternative_allele),
+KEY `fkIdx_28` (rs_id),
+CONSTRAINT `FK_28` FOREIGN KEY `fkIdx_28` (rs_id) REFERENCES RS (rs_id),
+CONSTRAINT `FK_38` FOREIGN KEY `fkIdx_38` (id_alternative_allele) REFERENCES ALTERNATIVE_ALLELE (id_alternative_allele)
 );
 
 
