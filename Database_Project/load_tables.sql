919e6cd065e856e9bc88ac74287d34da19430e22
PANGEAproject <43254659+PANGEAproject@users.noreply.github.com>
2018-11-29 11:04:05 -0600

    Update load_tables.sql
    documentacion

--- Database_Project/load_tables.sql
+++ Database_Project/load_tables.sql
@@ -1,7 +1,18 @@
-mysql -u -p --local-infile
+###########################################################################################
+
+                                                  #load_tables.sql
+
+############################################################################################
+
+#se inicia secion en sql con la opcion --local-infile para cargar archivos
+mysql -p --local-infile
+#se usa nuestra base de datos
 USE PANGEA_DB
 SET GLOBAL local_infile = "ON";
+#se inserta la tabla rs
 LOAD DATA LOCAL INFILE '/home/luisrs/storage/Project/bin/parsing_end.txt' INTO TABLE RS FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/home/luisrs/storage/Project/bin/parsing_end_2.txt' INTO TABLE FREQUENCY_RS_CONTINENT FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+#se inserta la tabla alelos alternativos
+LOAD DATA LOCAL INFILE '/home/luisrs/storage/Project/bin/parsing_end_alter.txt' INTO TABLE ALTERNATIVE_ALLELE FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+#se crea la tabla frecuencias
+LOAD DATA LOCAL INFILE '/home/luisrs/storage/Project/bin/parsing_end_freq.txt' INTO TABLE FREQUENCY_RS_CONTINENT FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 SET GLOBAL local_infile = "OFF";
